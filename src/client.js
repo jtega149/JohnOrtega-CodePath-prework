eb71fa7c254787b23bc1cffede00a732b848dd7a
John Ortega <j.tega149@gmail.com>
2026-01-07 15:14:41 -0500

    keys for codepath to see

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = import.meta.env.VITE_SUPABASE_URL;
-const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
+const URL = "https://oopemiitrawvjorggzdf.supabase.co";
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vcGVtaWl0cmF3dmpvcmdnemRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMzUwNTUsImV4cCI6MjA4MDcxMTA1NX0.fZTQTZ_r3XCaTdkU9K07x3E93RGxQfa5VKA45ERCv0s";
 
 export const supabase = createClient(URL, API_KEY);
